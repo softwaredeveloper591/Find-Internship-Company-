12a04674c280e024f563529874dac3008a209e68
Enes Sezer <116477172+softwaredeveloper591@users.noreply.github.com>
2024-05-27 07:28:13 +0300

    db changes

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const Sequlize = require("sequelize");
-const sequelize= new Sequlize("mysql://hpkp6jw607pp5buy:d7561shgxf487p1k@w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/e5asdqjuwe3vye7k",
+const sequelize= new Sequlize("mysql://sdv6k03sq9002wxv:utk6tfl4plobswj5@b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/o09uyv2tnnwb9utm",
 {
 	dialect: "mysql",
     logging: false
