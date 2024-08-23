7810168e1ede75b5ec4f191cd51225171236641c
Ahsabar <ahmetsaidbarkahan@gmail.com>
2024-08-23 22:19:15 +0300

    error handlers for company service
    first version of error handlers

---
+++ company/utils/customError.js
@@ -0,0 +1,15 @@
+class CustomError extends Error {
+	constructor(message, statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+		this.status = statusCode >= 400 && statusCode < 500 ? 'fail' : 'error';
+
+		this.isOperational = true;
+
+		Error.captureStackTrace(this);
+
+		console.log(message);
+	}
+}
+
+module.exports = CustomError;
