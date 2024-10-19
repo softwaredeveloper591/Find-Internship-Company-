56bc3560aa57f1c74fc018d83aae8dbd8c100cfd
Mehmet Anıl <m.cmert1907@hotmail.com>
2024-10-19 16:29:13 +0300

    admin routta ihtiyaç değişiklikler yapıldı
    admin routta ihtiyaç değişiklikler yapıldı

--- signup/index.js
+++ signup/index.js
@@ -5,7 +5,12 @@ const { PORT } = require('./config');
 const cors = require('cors');
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with the URL of your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    credentials: true, // Allow cookies if needed
+	// security headers
+}));
 app.use(express.urlencoded({extended:false})); // to obtain the data coming from forms in a json structure.
 app.use(express.json());
 
@@ -17,12 +22,7 @@ app.use(express.static("node_modules"));
 app.use(express.static("style"));
 
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5173', // Replace with the URL of your frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-    credentials: true, // Allow cookies if needed
-	// security headers
-}));
+
 
 app.set("view engine", "ejs");
 
