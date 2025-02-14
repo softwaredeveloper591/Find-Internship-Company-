e1701bcb16ba6c22ecdd9426a8387c6a88de8a4d
Enes Sezer <enessezer002@gmail.com>
2025-02-14 18:13:46 +0300

    messages with student

--- student/router/student.js
+++ student/router/student.js
@@ -30,6 +30,9 @@ const Document_model = require("../models/document-model");
 const Application_model = require("../models/application-model");
 const Internship_model = require("../models/internship-model");
 const Message_model = require("../models/message-model");
+const Conversation_model = require("../models/conversation-model");
+const Secretary_model = require("../models/secretary-model");
+
 
 /*let totalAnnouncementsCount = 0;
 
@@ -545,6 +548,202 @@ router.get("/download/:studentId/:fileType",[auth,checkUserRole("student")], asy
     res.setHeader('Content-Disposition', 'attachment; filename='+encodeURI(filename));
     res.setHeader('Content-Type', contentType);
     res.send(binaryData);
+}));
+
+
+router.get("/users", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const secretary = await Student_model.findAll({attributes: [ 'username', 'email']});
+	const students = await Student_model.findAll({attributes: [ 'username', 'email']});
+	const companies = await Company_model.findAll({attributes: [ 'username', 'email']});
+	const admin = await Admin_model.findAll({attributes: [ 'username', 'email']});
+	const allUsers = [...secretary, ...students, ...companies, admin];
+	res.status(200).json({ allUsers });
+}));
+
+router.get("/conversations", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const conversations = await Conversation_model.findAll({
+		where: {
+		  [Op.or]: [
+			{ user1_email: student.email, isDeletedByUser1: false },
+			{ user2_email: student.email, isDeletedByUser2: false }
+		  ]
+		},
+		attributes: ['id', 'user1_email', 'user1_name', 'user2_email', 'user2_name']
+	  });
+	res.status(200).json({ conversations });
+}));
+
+router.post("/conversations", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const { receiverEmail,receiverName } = req.body;
+	if (student.email === receiverEmail) {
+        return res.status(400).json({ error: "Users cannot create a conversation with themselves" });
+    }
+
+	const receiver = await findReceiverByEmail(receiverEmail);
+    if (!receiver) {
+        return res.status(400).json({ error: "Receiver email does not exist in the system" });
+    }
+
+	const existingConversation = await Conversation_model.findOne({
+        where: {
+            [Op.or]: [
+                { user1_email: student.email, user2_email: receiverEmail },
+                { user1_email: receiverEmail, user2_email: student.email }
+            ]
+        }
+    });
+
+    if (existingConversation) {
+		if(existingConversation.user1_email === student.email && existingConversation.isDeletedByUser1){
+			await existingConversation.update({ isDeletedByUser1: false });
+		}
+		else if(existingConversation.user2_email === student.email && existingConversation.isDeletedByUser2){
+			await existingConversation.update({ isDeletedByUser2: false });
+		}
+		else
+			return res.status(400).json({ error: "Conversation already exists" });
+		return res.status(200).json({ conversations: existingConversation });
+    }
+
+	const conversations = await Conversation_model.create({
+		user1_email: student.email,
+		user1_name: student.username,
+		user2_email: receiverEmail,
+		user2_name: receiverName
+	  },{
+        attributes: ['id', 'user1_email', 'user1_name','user2_email' ,'user2_name']
+    });
+	res.status(200).json({ conversations });
+}));
+
+router.get("/conversations/:id", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const conversationId= req.params.id;
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const conversation = await Conversation_model.findOne({ where: { id: conversationId } });
+
+    if (!conversation) {
+        return res.status(404).json({ error: "Conversation not found" });
+    }
+    if (![conversation.user1_email, conversation.user2_email].includes(student.email)) {
+        return res.status(403).json({ error: "You are not a participant in this conversation" });
+    }
+
+	const messages = await Message_model.findAll({
+        where: { conversation_id: conversationId },
+        order: [['createdAt', 'ASC']],
+        attributes: ['id', 'from', 'to', 'message', 'createdAt', 'fileName', 'data']
+    });
+
+	const unifiedMessages = messages.map(msg => ({
+        id: msg.id,
+        from: msg.from,
+        to: msg.to,
+        message: msg.message,
+        timestamp: msg.createdAt,
+        isSentByUser: msg.from === student.email,
+		fileName: msg.fileName,
+        data: msg.data ? msg.data.toString('base64') : null
+    }));
+
+    res.status(200).json({ messages: unifiedMessages });
+}));
+
+router.delete("/conversations/:id", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const conversationId= req.params.id;
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const conversation = await Conversation_model.findByPk(conversationId);
+
+    if (!conversation) {
+        throw new Error('Conversation not found');
+    }
+
+    let updateField, oppositeField;
+
+    if (conversation.user1_email === student.email) {
+        updateField = 'isDeletedByUser1';
+        oppositeField = 'isDeletedByUser2';
+    } else if (conversation.user2_email === student.email) {
+        updateField = 'isDeletedByUser2';
+        oppositeField = 'isDeletedByUser1';
+    } else {
+        throw new Error('User does not belong to this conversation');
+    }
+
+    if (conversation[oppositeField]) {
+        await conversation.destroy({ where: { id: conversationId } });
+    } else {
+        await conversation.update({ [updateField]: true });
+    }
+	res.status(200).json("Conversation deleted successfully");
+}));
+
+router.post("/sendMessage", upload.single('file'), [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const {conversationId, message } = req.body;
+	
+	const conversation = await Conversation_model.findOne({ where: { id: conversationId } });
+	if (!conversation) {
+		return res.status(404).json({ errors: "Conversation not found" });
+	}
+		
+	//user can't create messages in which it's not a part of the conversation
+	let receiverEmail;
+    if (conversation.user1_email === student.email) {
+        receiverEmail = conversation.user2_email;
+    } else if (conversation.user2_email === student.email) {
+        receiverEmail = conversation.user1_email;
+    } else {
+        return res.status(403).json({ error: "You are not a participant in this conversation" });
+    }
+
+	const receiver = await findReceiverByEmail(receiverEmail);
+	const file = req.file;
+	let fileName = null;
+	let data = null;
+
+	if (file) {
+		fileName = file.originalname;
+		data = file.buffer;
+	}
+
+	const createdMessage = await Message_model.create(
+		{
+			from: student.email,
+			senderName: student.username,
+			to: receiverEmail,
+			receiverName: receiver.username,
+			conversation_id: conversationId,
+			message,
+			fileName,
+			data,
+		}
+	);
+
+	// Send only the necessary parts of the message
+	res.status(200).json({
+		id: createdMessage.id,
+		receiver: createdMessage.receiverName,
+		message: createdMessage.message,
+	});
+}));
+
+router.delete("/deleteMessage/:id", [auth, checkUserRole("student")], asyncErrorHandler(async (req, res, next) => {
+	const id = req.params.id;
+	const student = await Student_model.findOne({ where: { id: req.user.id }, attributes: { exclude: ['password'] } });
+	const message = await Message_model.findOne({ where: { id } });
+	
+	if (!message) {
+        return res.status(404).json({ error: "Message not found with the given id" });
+    }
+	
+	if (message.from !== student.email && message.to !== student.email) {
+        return res.status(403).json({ error: "You are not authorized to delete this message!" });
+    }
+
+	await message.destroy();
+	res.status(200).json({ message: "Message deleted successfully", deletedMessage: message });
 }));
 
 module.exports= router;
