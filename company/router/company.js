56a291d539f8d6781c2f0db5f48d966822b5c3b6
Enes Sezer <enessezer002@gmail.com>
2025-02-21 14:19:11 +0300

    typo fix

--- company/router/company.js
+++ company/router/company.js
@@ -619,7 +619,7 @@ router.get("/conversations", [auth, checkUserRole("company")], asyncErrorHandler
 			{ user2_email: company.email, isDeletedByUser2: false }
 		  ]
 		},
-		attributes: ['id', 'user1_email', 'user1_name', 'user2_email', 'user2_name', 'user1_newMessages', 'user2_new_messages', 'last_message_time']
+		attributes: ['id', 'user1_email', 'user1_name', 'user2_email', 'user2_name', 'user1_new_messages', 'user2_new_messages', 'last_message_time']
 	  });
 
 	  const formattedConversations = conversations.map(conv => {
